0fda4a9c263d552c1f73efdfdb81a222f454e0af
Izabella Ribeiro <bellarpjf@gmail.com>
2018-09-06 10:24:07 -0300

    up

---
+++ src/model/Cliente.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String data_nascimento;
+    private String email;
+
+    public getNome(){
+        return nome;
+    }
+
+    public setNome(String nome){
+        return this.nome = nome;
+    }
+
+    public getSobrenome(){
+        return sobrenome;
+    }
+
+    public setSobrenome(String sobreome){
+        return this.sobrenome = sobrenome;
+    }
+
+    public getCpf(String cpf){
+        return cpf;
+    }
+
+    public setCpf(String cpf){
+        return this.
+    }
+}
