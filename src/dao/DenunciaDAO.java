9f5c897d82ad3721fd1a1b3eba366c2d6962ca1f
luis <luisguilhermeguimaraes@hotmail.com>
2018-09-20 08:16:20 -0300

    up DAO

--- src/dao/DenunciaDAO.java
+++ src/dao/DenunciaDAO.java
@@ -13,12 +13,18 @@ public class DenunciaDAO {
         PreparedStatement comando = null;
         try {
             conexao = BD.getConexao();
-            String sql = "insert into denuncia(id, descricao,autor)"
+            String sql = "insert into denuncia(id, descricao,autor,espaco)"
                     + " values(?,?,?)";
             comando = conexao.prepareStatement(sql);
             comando.setLong     (1, denuncia.getId());
             comando.setString   (2, denuncia.getDescricao());
             comando.setString   (3, denuncia.getAutor());
+
+            if(denuncia.getEspaco() == null){
+                comando.setNull(4, Types.NULL);
+            }else{
+                comando.setString(4,espaco.getId());
+            }
 
             comando.execute();
             BD.fecharConexao(conexao, comando);
@@ -32,12 +38,20 @@ public class DenunciaDAO {
         PreparedStatement comando = null;
         try {
             conexao = BD.getConexao();
-            String sql = "update denuncia set descricao=?, autor=? where id=?";
+            String sql = "update denuncia set descricao=?, autor=?, espaco=? where id=?";
 
             comando = conexao.prepareStatement(sql);
 
             comando.setString   (1, denuncia.getDescricao());
             comando.setString   (2, denuncia.getAutor());
+
+            if(denuncia.getEspaco() == null){
+                comando.setNull(3,Types.NULL);
+            }else{
+                comando.setString(3,espaco.getId());
+            }
+            comando.setLong(4,denuncia.getId());
+            BD.fecharConexao(conexao, comando);
         } catch (SQLException e) {
             throw e;
         }
@@ -48,7 +62,7 @@ public class DenunciaDAO {
         PreparedStatement comando = null;
         try {
             conexao = BD.getConexao();
-            String sql = "delete from denuncia where id=?";
+            String sql = "delete from denuncia where id=denuncia.getId()";
             comando = conexao.prepareStatement(sql);
             comando.setLong(1, denuncia.getId());
 
@@ -58,6 +72,8 @@ public class DenunciaDAO {
             return true;
         } catch (SQLException e) {
             throw e;
+        }finally {
+            BD.fecharConexao(conexao, comando);
         }
     }
 }
