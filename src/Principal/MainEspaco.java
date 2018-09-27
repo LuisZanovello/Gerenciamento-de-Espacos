5d93798f438ecf320aa656a8c0873a86a8ccf514
viictorw <viictor.wagner@gmail.com>
2018-09-26 23:44:17 -0300

    Troca de nome da Classe
    antes: Classe Endereço
    depois: Classe Espaço

---
+++ src/Principal/MainEspaco.java
@@ -0,0 +1,13 @@
+package principal;
+
+import model.Espaco;
+
+public class MainEspaco {
+
+    public static void main(String[] args) {
+        Espaco espc = new Espaco((long)123,"SportMania" ,"1234567890" ,"36045000",
+                "Logradouro" ,1 ,"compl" ,"Bairro" ,
+                "Cidade","UF" ,80.5,900 ,
+                "6h00","23h00" );
+    }
+}
