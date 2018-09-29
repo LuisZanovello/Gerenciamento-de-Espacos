701fad3338569cbba2b5e4eada58a5663ba7d79c
viictorw <viictor.wagner@gmail.com>
2018-09-29 16:14:28 -0300

    Controller
    Não deveria se chamar principal ..
    "padrão MDC :
    Model DAO Controller"
    **Editem as suas classes main pra dentro do pacote "controller"

---
+++ src/controller/controllerAdmin.java
@@ -0,0 +1,15 @@
+package controller;
+
+import dao.AdministradorDAO;
+import model.Administrador;
+import java.sql.SQLException;
+
+public class controllerAdmin {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Administrador admin = new Administrador((long)132,"Victor", "viictor.wagmer@lul.br", "123123");
+                                                /*ID        NOME            EMAIL                           SENHA*/
+        AdministradorDAO.gravar(admin);
+
+    }
+}
