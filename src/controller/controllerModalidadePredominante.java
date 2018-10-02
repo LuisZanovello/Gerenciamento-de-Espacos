660cbba8edd35afa9396d6f1ef934a7c435a7848
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-02 02:48:29 -0300

    controller atualizado

--- src/controller/controllerModalidadePredominante.java
+++ src/controller/controllerModalidadePredominante.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 public class controllerModalidadePredominante {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ModalidadePredominante modalidade = new ModalidadePredominante((long)123,"aaa","iiii");
+        ModalidadePredominante modalidade = new ModalidadePredominante((long)124,"bbb","ooooo");
 
-        System.out.println(modalidade.getModalidadePredominante());
 
         ModalidadePredominanteDAO.gravar(modalidade);
+
     }
 }
