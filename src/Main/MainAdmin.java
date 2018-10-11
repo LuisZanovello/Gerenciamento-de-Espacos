cb5b6ae6ccf8b6164701bfe2b51956700f6ae561
viictorw <viictor.wagner@gmail.com>
2018-10-11 09:43:51 -0300

    att

--- src/Main/MainAdmin.java
+++ src/Main/MainAdmin.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class MainAdmin {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        /* Administrador admin = new Administrador((long)8,"Victor", "viictor.wagner@lul.br", "123123");
-        Administrador admin = new Administrador((long)9,"Luis", "luis.zano@lul.br", "321321");
+        Administrador admin = new Administrador((long)8,"Victor", "viictor.wagner@lul.br", "123123");
+        /*Administrador admin = new Administrador((long)9,"Luis", "luis.zano@lul.br", "321321");
         Administrador admin = new Administrador((long)10,"Iza", "iza.ribe@lul.br", "123412");
         Administrador admin = new Administrador((long)11, "WW", "ww.w@lul.com.br", "456321");*/
                                                 /*ID        NOME            EMAIL                           SENHA*/
@@ -16,12 +16,12 @@ public class MainAdmin {
 
 
         //obter todos os administradores
-       /* for(int x=0;x<=3;x++) {
+
+        for(int x=0;x<=3;x++) {
             System.out.println(AdministradorDAO.obterTodosAdministradores().get(x).getNome());
         }
-*/
 
-           System.out.println(AdministradorDAO.obterAdministrador((long) 8).getNome());
+
 
 
     }
