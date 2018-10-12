feda7539cee607fb39a5b3bd5fc4c21b870450a8
viictorw <viictor.wagner@gmail.com>
2018-10-12 11:12:30 -0300

    att ''correção'' de leve
    Co-Authored-By: izaribeiro <izaribeiro@users.noreply.github.com>

--- src/controller/PesquisaClienteController.java
+++ src/controller/PesquisaClienteController.java
@@ -1,8 +1,7 @@
 package controller;
 
-import dao.ClienteDAO;
-import model.Administrador;
 import model.Cliente;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,26 +12,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "PesquisaClienteController")
-public class PesquisaClienteController extends HttpServlet {
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+@WebServlet(name = "PesquisaClienteController", urlPatterns = {"/PesquisaClienteController"})
+public class    PesquisaClienteController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        try{
-            request.setAttribute("clientes", Cliente.obterClientes());
-            RequestDispatcher view = request.getRequestDispatcher("/pesquisaCliente.jsp");
-            view.forward(request, response);
-        }catch (ClassNotFoundException e){
-            throw new ServletException(e);
-        }catch (SQLException e){
-            throw new ServletException(e);
-        }catch (ServletException e){
-            e.printStackTrace();
-        }
+        processRequest(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request,response );
+        processRequest(request, response);
     }
 
-
+    protected void processRequest(HttpServletRequest request,
+                                  HttpServletResponse response) throws SecurityException, IOException, ServletException {
+        try {
+            request.setAttribute("clientes", Cliente.obterTodosOsClientes());
+            RequestDispatcher view = request.getRequestDispatcher("/pesquisaCliente.jsp");
+            view.forward(request, response);
+        } catch (ClassNotFoundException e) {
+            throw new ServletException(e);
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/controller/PesquisaContatoController.java
+++ src/controller/PesquisaContatoController.java
@@ -1,29 +1,46 @@
 package controller;
 
+import model.Contato;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-public class PesquisaContatoController {
+@WebServlet(name = "PesquisaContatoController", urlPatterns = {"/PesquisaContatoController"})
+public class    PesquisaContatoController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        try{
-            request.setAttribute("clientes", Cliente.obterClientes());
-            RequestDispatcher view = request.getRequestDispatcher("/pesquisaCliente.jsp");
-            view.forward(request, response);
-        }catch (ClassNotFoundException e){
-            throw new ServletException(e);
-        }catch (SQLException e){
-            throw new ServletException(e);
-        }catch (ServletException e){
-            e.printStackTrace();
-        }
+        processRequest(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request,response );
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request,
+                                  HttpServletResponse response) throws SecurityException, IOException, ServletException {
+        try {
+            request.setAttribute("contatos", Contato.obterTodosEspacos());
+            RequestDispatcher view = request.getRequestDispatcher("/pesquisaContato.jsp");
+            view.forward(request, response);
+        } catch (ClassNotFoundException e) {
+            throw new ServletException(e);
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
     }
 }
