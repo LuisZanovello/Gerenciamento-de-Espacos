23c21def7ef1fb5794ddca7ea7b268a2dea9e40b
viictorw <viictor.wagner@gmail.com>
2018-10-13 12:42:14 -0300

    Classe DAO Cliente da Iza de volta
    A classe esta igual a anterior com a ajuda do Allan para reduzir digitação e repetição de info
    
    Co-Authored-By: izaribeiro <izaribeiro@users.noreply.github.com>

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -81,18 +81,7 @@ public class ClienteDAO {
             comando.setLong(1, id);
             ResultSet rs = comando.executeQuery();
             rs.first();
-
-            /* antes
             cliente = getClient(rs);
-            */
-
-            cliente = new Cliente(rs.getLong("id"),
-                    rs.getString("nome"),
-                    rs.getString("sobrenome"),
-                    rs.getString("data_nascimento"),
-                    rs.getString("email"),
-                    rs.getString("cpf"));
-
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -101,8 +90,6 @@ public class ClienteDAO {
         return cliente;
     }
 
-/*
-    Isso esta ajudando e atrapalhando
     private static Cliente getClient(ResultSet rs) throws SQLException {
         return new Cliente(rs.getLong("id"),
                 rs.getString("nome"),
@@ -117,7 +104,6 @@ public class ClienteDAO {
         Connection conexao = null;
         Statement comando = null;
         ArrayList<Cliente> clientes = new ArrayList<>();
-
         try {
             conexao = BD.getConexao();
             comando = conexao.createStatement();
@@ -126,35 +112,11 @@ public class ClienteDAO {
             while (rs.next()) {
                 clientes.add(getClient(rs));
             }
-
-            */
-
-    public static ArrayList<Cliente> obterTodosOsClientes() throws ClassNotFoundException{
-        Connection conexao = null;
-        Statement comando = null;
-        ArrayList<Cliente> lista = new ArrayList<>();
-        Cliente cliente = null;
-        try {
-            conexao = BD.getConexao();
-            comando = conexao.createStatement();
-            String sql = "SELECT * FROM cliente";
-            ResultSet rs = comando.executeQuery(sql);
-
-            while (rs.next()) {
-
-                cliente = new Cliente(rs.getLong("id"),
-                        rs.getString("nome"),
-                        rs.getString("sobrenome"),
-                        rs.getString("data_nascimento"),
-                        rs.getString("email"),
-                        rs.getString("cpf"));
-                lista.add(cliente);
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             BD.fecharConexao(conexao, comando);
-            return lista;
+            return clientes;
         }
 
     }
