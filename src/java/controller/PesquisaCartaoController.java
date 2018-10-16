694595087f4273cf0791e74cb2705dd6b1d14bef
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-16 19:25:50 -0300

    att cartao.jsp

--- src/java/controller/PesquisaCartaoController.java
+++ src/java/controller/PesquisaCartaoController.java
@@ -2,13 +2,6 @@ package controller;
 
 import model.Cartao;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,11 +13,13 @@ import java.sql.SQLException;
 
 @WebServlet(name = "PesquisaCartaoController", urlPatterns = {"/PesquisaCartaoController"})
 public class    PesquisaCartaoController extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         processRequest(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -35,12 +30,10 @@ public class    PesquisaCartaoController extends HttpServlet {
             request.setAttribute("cartoes", Cartao.obterTodosEspacos());
             RequestDispatcher view = request.getRequestDispatcher("/pesquisaCartao.jsp");
             view.forward(request, response);
-        } catch (ClassNotFoundException e) {
-            throw new ServletException(e);
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             throw new ServletException(e);
         } catch (ServletException e) {
-            e.printStackTrace();
         }
     }
+   
 }
