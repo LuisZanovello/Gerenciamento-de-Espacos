a21742af81bd07ccf1a08673614235b4bbe9af66
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-19 21:42:39 -0300

    update pesquisaContatoController

--- src/java/controller/PesquisaContatoController.java
+++ src/java/controller/PesquisaContatoController.java
@@ -3,7 +3,6 @@ package controller;
 import model.Contato;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import java.sql.SQLException;
 
-@WebServlet(name = "PesquisaContatoController", urlPatterns = {"/PesquisaContatoController"})
 public class    PesquisaContatoController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
