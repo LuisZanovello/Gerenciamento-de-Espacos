2604433676a768d89a28219d5656b4737c91e546
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-20 13:16:30 -0300

    update pesquisaReservaController

--- src/java/controller/PesquisaReservaController.java
+++ src/java/controller/PesquisaReservaController.java
@@ -1,33 +1,26 @@
 package controller;
 
-import model.Pagamento;
-import model.Reembolso;
 import model.Reserva;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "PesquisaReservaController", urlPatterns = {"/PesquisaReservaController"})
 public class PesquisaReservaController extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         processRequest(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -38,9 +31,7 @@ public class PesquisaReservaController extends HttpServlet {
             request.setAttribute("reservas", Reserva.obterTodasReservas());
             RequestDispatcher view = request.getRequestDispatcher("/pesquisaReserva.jsp");
             view.forward(request, response);
-        } catch (ClassNotFoundException e) {
-            throw new ServletException(e);
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             throw new ServletException(e);
         } catch (ServletException e) {
             e.printStackTrace();
