9e2a04c1676ce9da92cd5135f3e3570a2ac5c07a
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-19 21:44:35 -0300

    update pesquisaPagamentoController

--- src/java/controller/PesquisaPagamentoController.java
+++ src/java/controller/PesquisaPagamentoController.java
@@ -2,30 +2,22 @@ package controller;
 
 import model.Pagamento;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "PesquisaPagamentoController", urlPatterns = {"/PesquisaPagamentoController"})
 public class PesquisaPagamentoController extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         processRequest(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -36,9 +28,7 @@ public class PesquisaPagamentoController extends HttpServlet {
             request.setAttribute("pagamentos", Pagamento.obterTodosPagamentos());
             RequestDispatcher view = request.getRequestDispatcher("/pesquisaPagamento.jsp");
             view.forward(request, response);
-        } catch (ClassNotFoundException e) {
-            throw new ServletException(e);
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             throw new ServletException(e);
         } catch (ServletException e) {
             e.printStackTrace();
