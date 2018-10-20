989c42863068939b3618e3ce06000698a314317b
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-19 23:01:42 -0300

    update mainPagamento

--- src/java/main/MainPagamento.java
+++ src/java/main/MainPagamento.java
@@ -4,18 +4,22 @@ import dao.PagamentoDAO;
 import model.Pagamento;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MainPagamento {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
        // Pagamento pag = new Pagamento((long)3, "01/10/2018", (long)5555, (double)150.00, (long)33);
 
-        Pagamento pag = new Pagamento((long)2, "01/10/2018", (long)5555, (double)250.00, (long)11);
+       // Pagamento pag = new Pagamento((long)2, "01/10/2018", (long)5555, (double)250.00, (long)123);
       //  Pagamento pag = new Pagamento((long)1, "01/10/2018", (long)8888, (double)150.00, (long)55);
 
 
 
 
-        PagamentoDAO.gravar(pag);
-    }
+        //PagamentoDAO.gravar(pag);
+
+  
+     System.out.println(PagamentoDAO.obterTodosPagamentos().get(0));
+}
 }
