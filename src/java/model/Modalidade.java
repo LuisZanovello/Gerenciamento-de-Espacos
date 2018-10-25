9dace88951fc39711025e1f72540d61ab08b4a65
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-24 22:48:01 -0300

    update Modalidade model

--- src/java/model/Modalidade.java
+++ src/java/model/Modalidade.java
@@ -5,6 +5,10 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Modalidade {
+
+    public static Modalidade obterTodasModalidades(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     private Long id;
     private String modalidade;
     private String descricao;
