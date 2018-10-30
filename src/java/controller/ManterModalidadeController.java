e6d8c4529f2f8bd8b79c1ecb81e7195a2a9c40cc
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-30 01:12:29 -0300

    update ManterModalidadeController

--- src/java/controller/ManterModalidadeController.java
+++ src/java/controller/ManterModalidadeController.java
@@ -11,7 +11,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,7 +20,6 @@ import model.Modalidade;
  *
  * @author luisg
  */
-//@WebServlet(name = "ManterModalidadeController_1", urlPatterns = {"/ManterModalidadeController_1"})
 public class ManterModalidadeController extends HttpServlet {
 
     /**
@@ -36,17 +34,16 @@ public class ManterModalidadeController extends HttpServlet {
      * @throws java.lang.ClassNotFoundException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException, SQLException, ClassNotFoundException {
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
         String acao = request.getParameter("acao");
         if (acao.equals("confirmarOperacao")) {
-            confirmarOperacao(request, response);
+            //  confirmarOperacao(request, response);
         } else {
             if (acao.equals("prepararOperacao")) {
                 prepararOperacao(request, response);
             }
         }
-        }
-    
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -62,9 +59,7 @@ public class ManterModalidadeController extends HttpServlet {
             throws ServletException, IOException {
         try {
             processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(ManterModalidadeController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(ManterModalidadeController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -82,9 +77,7 @@ public class ManterModalidadeController extends HttpServlet {
             throws ServletException, IOException {
         try {
             processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(ManterModalidadeController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(ManterModalidadeController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -99,20 +92,22 @@ public class ManterModalidadeController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
- private void prepararOperacao(HttpServletRequest request, HttpServletResponse response) throws SQLException, ClassNotFoundException, IOException, ServletException {
-        String operacao = request.getParameter("operacao");
-        request.setAttribute("operacao", operacao);
-        request.setAttribute("modalidades", Modalidade.obterTodasModalidades());
-        if (!operacao.equals("Incluir")) {
-            long id = Integer.parseInt(request.getParameter("id"));
-            Modalidade modalidade = Modalidade.obterTodasModalidades(id);
-            request.setAttribute("modalidade", modalidade);
+    private void prepararOperacao(HttpServletRequest request, HttpServletResponse response) throws SQLException, ClassNotFoundException, IOException, ServletException {
+        try {
+            String operacao = request.getParameter("operacao");
+            request.setAttribute("operacao", operacao);
+            request.setAttribute("modalidades", Modalidade.obterTodasModalidades());
+            if (!operacao.equals("Incluir")) {
+                long id = Long.parseLong(request.getParameter("id").trim());
+                Modalidade modalidade = Modalidade.obterModalidade((long) id);
+                request.setAttribute("modalidade", modalidade);
+            }
+            RequestDispatcher view = request.getRequestDispatcher("/manterModalidade.jsp");
+            view.forward(request, response);
+        } catch (ServletException e) {
+            throw e;
+        } catch (IOException | SQLException | ClassNotFoundException e) {
+            throw new ServletException(e);
         }
-        RequestDispatcher view = request.getRequestDispatcher("/manterModalidade.jsp");
-        view.forward(request, response);
-    }
-
-    private void confirmarOperacao(HttpServletRequest request, HttpServletResponse response) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
