e7c5c14af96702f63dee17356a06431d68d07a94
luis <luisguilhermeguimaraes@hotmail.com>
2018-10-30 01:16:29 -0300

    update pesquisaEspacoController

--- src/java/controller/PesquisaEspacoController.java
+++ src/java/controller/PesquisaEspacoController.java
@@ -2,43 +2,33 @@ package controller;
 
 import model.Espaco;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "PesquisaEspacoController", urlPatterns = {"/PesquisaEspacoController"})
+
 public class    PesquisaEspacoController extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         processRequest(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    protected void processRequest(HttpServletRequest request,
-                                  HttpServletResponse response) throws SecurityException, IOException, ServletException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws SecurityException, IOException, ServletException {
         try {
             request.setAttribute("espacos", Espaco.obterTodosEspacos());
             RequestDispatcher view = request.getRequestDispatcher("/pesquisaEspaco.jsp");
             view.forward(request, response);
-        } catch (ClassNotFoundException e) {
-            throw new ServletException(e);
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             throw new ServletException(e);
         } catch (ServletException e) {
             e.printStackTrace();
