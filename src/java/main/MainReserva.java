d236dbfec082442e1b63f6cdb983196f3d7d4430
luis <luisguilhermeguimaraes@hotmail.com>
2018-12-06 01:45:49 -0200

    Update MainReserva.java

--- src/java/main/MainReserva.java
+++ src/java/main/MainReserva.java
@@ -9,13 +9,13 @@ public class MainReserva {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         Reserva resv = new Reserva((long)33, "25/01/1995", "08:00", "22:00", (long)55,
-                (double)150.00, (long)5, (long)1,(long)125);
+                (double)150.00, (long)5,(long)125,(long)125);
         /*
          Reserva resv = new Reserva((long)55, "25/01/1995", "08:00", "22:00", (long)95,
-                (double)150.00, (long)5, (long)124,(long)124);
+                (double)150.00, (long)5,(long)125,(long)124);
 
         Reserva resv = new Reserva((long)11, "25/01/1995", "08:00", "22:00", (long)150,
-                (double)250.00, (long)4, (long)2,(long)126);
+                (double)250.00, (long)4,(long)125,(long)126);
 */
         ReservaDAO.gravar(resv);
     }
